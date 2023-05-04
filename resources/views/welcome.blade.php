b611232d876dadde0f47593fbd26c9da5072eeeb
Rostyslav Fedoriv <froizerner@gmail.com>
2023-05-05 01:22:40 +0300

    Created base layout
    Created base layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
 </head>
-<body>
-<h1 class="uppercase">
-    Hello world!
-</h1>
+<body class="bg-gradient-to-b from-blue-500/80  to-yellow-500/80">
+<div class="max-w-screen-xl mx-auto h-screen grid grid-rows-[auto-1fr-auto]">
+    <header class="px-2 border-b h-14 flex items-center justify-between">
+            <a href="" class="uppercase font-bold text-2xl text-white hover:text-amber-300">Uanews</a>
+            <nav class="flex space-x-5">
+                <ul class="text-white flex space-x-1">
+                    <li><a href="" class="header-link">Home</a></li>
+                    <li><a href="" class="header-link">Posts</a></li>
+                </ul>
+
+            </nav>
+    </header>
+    <div>
+        <main>
+Hello word
+        </main>
+    </div>
+
+    <footer class="px-2 border-t">
+        <div class="">
+            <div>
+                <h6>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </h6>
+            </div>
+        </div>
+    </footer>
+</div>
 </body>
 </html>
